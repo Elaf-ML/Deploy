15b5bd566f5aac21b3c5d4aca6d85fcb6dc3fb08
Elaf Alzoubi <70773003+Elaf-ML@users.noreply.github.com>
2025-03-12 16:53:46 +0100

    Update LogInPage.tsx

--- backend/frontend1/src/pages/auth/login/LogInPage.tsx
+++ backend/frontend1/src/pages/auth/login/LogInPage.tsx
@@ -17,7 +17,7 @@ const queryClient = useQueryClient();
     const { mutate: loginMutation, isPending,isError, error } = useMutation({
         mutationFn: async ({ username, password }: { username: string; password: string }) => {
             try {
-                const res = await fetch("http://localhost:5000/api/auth/login", {
+                const res = await fetch("https://deploy-ompa.onrender.com/api/auth/login", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json",
