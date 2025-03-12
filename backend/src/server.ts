34e1e8561fe32051b3ec2d41bde104c98d7fe897
Elaf Alzoubi <70773003+Elaf-ML@users.noreply.github.com>
2025-03-12 15:06:59 +0100

    Update server.ts

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -25,7 +25,7 @@ mongoose.connect(process.env.MONGO_URI as string, {
 })
 .then(() => {
     console.log('Connected to MongoDB');
-    app.listen(PORT, () => {
+    app.listen(PORT,'0.0.0.0', () => {
         console.log(`Server is running on http://localhost:${PORT}`);
     });
 })
